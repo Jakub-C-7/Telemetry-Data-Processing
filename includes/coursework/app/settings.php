b14b47622b0909a5990228807a6540cb7eb48a4c
Jakub Chamera <P2409490@my365.dmu.ac.uk>
2021-11-30 11:38:48 +0000

    Added SOAP server settings and login details.

--- includes/coursework/app/settings.php
+++ includes/coursework/app/settings.php
@@ -19,35 +19,55 @@ define('CSS_PATH', $css_path);
 define('LOG_FILE_PATH', $log_file_path);
 define('APP_NAME', 'Telemetry Data Processing Coursework');
 
+/* M2M service configurations */
+//define('M2M_MSISDN', );
+//define('M2M_USERNAME', '');
+//define('M2M_PASSWORD', '');
+//$wsdl = 'https://m2mconnect.ee.co.uk/orange-soap/services/MessageServiceByCountry?wsdl';
+//define('WSDL', $wsdl);
 
 $settings = [
-  "settings" => [
-    'displayErrorDetails' => true,
-    'addContentLengthHeader' => false,
-    'mode' => 'development',
-    'debug' => true,
-    'view' => [
-      'template_path' => __DIR__ . '/templates/',
-      'twig' => [
-        'cache' => false,
-        'auto_reload' => true,
-      ]],
-    'pdo_settings' => [
-      'rdbms' => 'mysql',
-      'host' => 'localhost',
-      'db_name' => 'session_db',
-      'port' => '3306',
-      'user_name' => 'session_user',
-      'user_password' => 'session_user_pass',
-      'charset' => 'utf8',
-      'collation' => 'utf8_unicode_ci',
-      'options' => [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => true,
-      ],
-    ]
-  ],
+    "settings" => [
+        'displayErrorDetails' => true,
+        'addContentLengthHeader' => false,
+        'mode' => 'development',
+        'debug' => true,
+        'view' => [
+            'template_path' => __DIR__ . '/templates/',
+            'twig' => [
+                'cache' => false,
+                'auto_reload' => true,
+            ]],
+        'pdo_settings' => [
+            'rdbms' => 'mysql',
+            'host' => 'localhost',
+            'db_name' => 'session_db',
+            'port' => '3306',
+            'user_name' => 'session_user',
+            'user_password' => 'session_user_pass',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'options' => [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => true,
+            ],
+        ],
+        'soap' => [
+            'connection' => [
+                'wsdl' => 'https://m2mconnect.ee.co.uk/orange-soap/services/MessageServiceByCountry?wsdl',
+                'options' => [
+                    'trace' => true,
+                    'exceptions' => true
+                ]
+            ],
+            'login' => [
+                'username' => '21_2409490',
+                'password' => 'M2mJSM2021swad!'
+            ]
+        ]
+    ],
+
 ];
 
 return $settings;
