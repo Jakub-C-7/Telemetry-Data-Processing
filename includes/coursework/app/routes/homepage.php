292c6f5d82418f967838c3db32f86f29215a0584
Jakub Chamera <P2409490@my365.dmu.ac.uk>
2021-12-14 10:41:13 +0000

    Inserted the xml parser and created method 'processMessage' that takes an xml input, picks it apart, and returns a processed message array. A foreach loop is used to loop through the message list and parse each element in the message.

--- includes/coursework/app/routes/homepage.php
+++ includes/coursework/app/routes/homepage.php
@@ -13,19 +13,25 @@ use \Psr\Http\Message\ResponseInterface as Response;
 $app->get('/', function(Request $request, Response $response) use ($app) {
 
     $messageModel = $this->get('messageModel');
-//    $xmlParser = $this->get('xmlParser');
+    $xmlParser = $this->get('xmlParser');
 
     //Calls the method to download messages. The first field takes the username and the second takes number of messages.
-    $message_list = $messageModel->downloadMessages('', 15);
-    var_dump($message_list);
+    $message_list = $messageModel->downloadMessages('', 20);
 
-    //calls the createMessageDisplay method that then calls the twig that loops through the message list and spits it out onto the screen.
-    createMessageDisplay($app, $response, $message_list);
+    $parsed_message_list = [];
+    //Process message content for each retrieved message
+    foreach($message_list as $message){
+        $message = $xmlParser->parseXmlArray($message);
+        $parsed_message_list[] = processMessage($message);
+    }
+
+    //calls the createMessageDisplay method that then calls the twig that loops through the message list and displays messages
+    createMessageDisplay($app, $response, $parsed_message_list);
 
 })->setName('homepage');
 
 
-function createMessageDisplay($app, $response, $message_list): void
+function createMessageDisplay($app, $response, $parsed_message_list): void
 {
     $view = $app->getContainer()->get('view');
     $view->render($response,
@@ -40,9 +46,67 @@ function createMessageDisplay($app, $response, $message_list): void
             'page_heading_3' => 'Message Metadata',
             'page_heading_4' => 'Message Content',
             'info_text' => 'Your information will be stored in either a session file or in a database',
-            'sid_text' => ' ',
             'method' => 'post',
-            'message_list' => $message_list,
+            'message_list' => $parsed_message_list,
             'page_text' => 'Select a message',
         ]);
+}
+
+//Process XML retrieved from SOAP call
+function processMessage(array $message): array
+{
+    //Creating the processes message array to store messages.
+    $processedMessage = [
+        'source' => $message['SOURCEMSISDN'],
+        'destination' => $message['DESTINATIONMSISDN'],
+        'bearer' => $message['BEARER'],
+        'ref' => $message['MESSAGEREF']
+    ];
+
+    $receivedTime = ($message['RECEIVEDTIME']);
+    $processedMessage['received'] = $receivedTime;
+
+    if (isset($message['TMP'])) {
+        $processedMessage['temperature'] = $message['TMP'];
+    }else{
+        $processedMessage['temperature'] = 'EMPTY DATA';
+    }
+
+    if (isset($message['KP'])) {
+        $processedMessage['keypad'] = $message['KP'];
+    }else{
+        $processedMessage['keypad'] = 'EMPTY DATA';
+    }
+
+    if (isset($message['FN'])) {
+        $processedMessage['fan'] = $message['FN'];
+    }else{
+        $processedMessage['fan'] = 'EMPTY DATA';
+    }
+
+    if (isset ($message['SW1'])){
+        $processedMessage['switchOne'] = $message['SW1'];
+    }else{
+        $processedMessage['switchOne'] = 'EMPTY DATA';
+    }
+
+    if (isset ($message['SW2'])){
+        $processedMessage['switchTwo'] = $message['SW2'];
+    }else{
+        $processedMessage['switchTwo'] = 'EMPTY DATA';
+    }
+
+    if (isset ($message['SW3'])){
+        $processedMessage['switchThree'] = $message['SW3'];
+    }else{
+        $processedMessage['switchThree'] = 'EMPTY DATA';
+    }
+
+    if (isset ($message['SW4'])){
+        $processedMessage['switchFour'] = $message['SW4'];
+    }else{
+        $processedMessage['switchFour'] = 'EMPTY DATA';
+    }
+
+    return $processedMessage;
 }
