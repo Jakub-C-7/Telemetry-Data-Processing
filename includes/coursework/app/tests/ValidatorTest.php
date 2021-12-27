761494790d6e331f2ff0723c101b9b02027d6288
Jakub Chamera <P2409490@my365.dmu.ac.uk>
2021-12-27 21:32:12 +0000

    Finished off Temperature validation unit testing and completed keypad validation unit testing.

--- includes/coursework/app/tests/ValidatorTest.php
+++ includes/coursework/app/tests/ValidatorTest.php
@@ -15,7 +15,20 @@ class ValidatorTest extends TestCase
         $this->assertTrue($validator->validateTemperature($input));
     }
 
-    public function testValidatorTemperatureOutOfRangeHigh()
+    public function testValidatorTemperatureEmpty()
+    {
+        $validator = new Validator();
+
+        $input = '';
+
+        $this->assertFalse($validator->validateTemperature($input));
+
+        $errors = $validator->getErrors();
+
+        $this->assertEquals('Empty temperature value', $errors['temperature']);
+    }
+
+    public function testValidatorTemperatureOutOfUpperRange()
     {
         $validator = new Validator();
 
@@ -25,10 +38,10 @@ class ValidatorTest extends TestCase
 
         $errors = $validator->getErrors();
 
-        $this->assertEquals('Invalid temperature range', $errors['temperature']);
+        $this->assertEquals('Invalid temperature value range', $errors['temperature']);
     }
 
-    public function testValidatorTemperatureOutOfRangeLow()
+    public function testValidatorTemperatureOutOfLowerRange()
     {
         $validator = new Validator();
 
@@ -38,6 +51,89 @@ class ValidatorTest extends TestCase
 
         $errors = $validator->getErrors();
 
-        $this->assertEquals('Invalid temperature range', $errors['temperature']);
+        $this->assertEquals('Invalid temperature value range', $errors['temperature']);
+    }
+
+    public function testValidatorTemperatureInvalidLength()
+    {
+        $validator = new Validator();
+
+        $input = '--40';
+
+        $this->assertFalse($validator->validateTemperature($input));
+
+        $errors = $validator->getErrors();
+
+        $this->assertEquals('Invalid temperature value length', $errors['temperature']);
+    }
+
+    public function testValidatorTemperatureZeroString()
+    {
+        $validator = new Validator();
+
+        $input = '0';
+
+        $this->assertTrue($validator->validateTemperature($input));
+    }
+
+    public function testValidatorTemperatureInvalidInputType()
+    {
+        $validator = new Validator();
+
+        $input = 'a';
+
+        $this->assertFalse($validator->validateTemperature($input));
+
+        $errors = $validator->getErrors();
+
+        $this->assertEquals('Non-numeric temperature value', $errors['temperature']);
+    }
+
+    public function testValidatorKeypadCorrect()
+    {
+        $validator = new Validator();
+
+        $input = '5';
+
+        $this->assertTrue($validator->validateKeypad($input));
+    }
+
+    public function testValidatorKeypadEmpty()
+    {
+        $validator = new Validator();
+
+        $input = '';
+
+        $this->assertFalse($validator->validateKeypad($input));
+
+        $errors = $validator->getErrors();
+
+        $this->assertEquals('Empty keypad value', $errors['keypad']);
+    }
+
+    public function testValidatorKeypadOutOfUpperRange()
+    {
+        $validator = new Validator();
+
+        $input = '12';
+
+        $this->assertFalse($validator->validateKeypad($input));
+
+        $errors = $validator->getErrors();
+
+        $this->assertEquals('Invalid keypad value length', $errors['keypad']);
+    }
+
+    public function testValidatorKeypadInvalidInputType()
+    {
+        $validator = new Validator();
+
+        $input = '@';
+
+        $this->assertFalse($validator->validateKeypad($input));
+
+        $errors = $validator->getErrors();
+
+        $this->assertEquals('Invalid keypad value', $errors['keypad']);
     }
 }
