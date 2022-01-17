3d5c2542a439aa82c1b9dc89088fa7a10d6faabe
Jakub Chamera <P2409490@my365.dmu.ac.uk>
2022-01-17 09:01:22 +0000

    Extended the index.php docblock and added a newline

--- public_php/Coursework_main/coursework_public/index.php
+++ public_php/Coursework_main/coursework_public/index.php
@@ -1,9 +1,14 @@
 <?php
+
 /**
- * index.php
- * Calls the bootstrap file to start the application
+ * index.php script
  *
+ * PHP application made by group AA for downloading, parsing, validating, storing, displaying, and sending telemetry
+ * data.
+ *
+ * Calls the bootstrap file to start the application.
  *
  */
 
 include 'coursework/bootstrap.php';
+
